4950e370df9781956221ae232b3967761caec529
MarceloFigs <marcelofigueiredo.dev@gmail.com>
2023-02-23 11:02:01 -0300

    feat: adding caching service

---
+++ Services/CachingService.cs
@@ -0,0 +1,8 @@
+﻿using FoodDelivery.Services.Interfaces;
+
+namespace FoodDelivery.Services
+{
+    public class CachingService : ICachingService
+    {
+    }
+}
---
+++ Services/Interfaces/ICachingService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace FoodDelivery.Services.Interfaces
+{
+    public interface ICachingService
+    {
+        Task SetAsync(string key, string value);
+        Task<string> GetAsync(string key);
+    }
+}
